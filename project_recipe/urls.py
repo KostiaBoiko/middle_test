d8e7cfec493569b10c8efa5ecb982eb33d41bce3
KostiaBoiko <boikokostia@knu.ua>
2023-06-02 12:49:21 +0300

    created recipe detail

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -5,6 +5,6 @@ from recipe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', views.main_view, name='main'),
+    path('', views.main_view, name='main'),
     path('recipe_detail/<int:recipe_id>/', views.recipe_detail_view, name='recipe_detail'),
 ]
