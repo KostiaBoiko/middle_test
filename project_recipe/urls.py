f7944f4b56c2bfa9bdf57b8970e379d4fb0bef06
KostiaBoiko <boikokostia@knu.ua>
2023-06-02 12:45:47 +0300

    created views

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from recipe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', views.main_view, name='main'),
+    path('recipe_detail/<int:recipe_id>/', views.recipe_detail_view, name='recipe_detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+
+def main_view(request):
+    # Отримати всі рецепти за 2023 рік
+    recipes = Recipe.objects.filter(created_at__year=2023)
+    context = {'recipes': recipes}
+    return render(request, 'main.html', context)
+
+def recipe_detail_view(request, recipe_id):
+    # Отримати певний рецепт за його id
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    context = {'recipe': recipe}
+    return render(request, 'recipe_detail.html', context)
 
